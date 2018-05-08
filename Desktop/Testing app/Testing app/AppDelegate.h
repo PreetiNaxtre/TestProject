6d3283b3dc8936128038e5d0f26561eb8027dbf5
wrink@vrinno.com <i0UrpMWMRf8o2^#FUyN$I$cL>
2018-05-08 10:32:28 +0530

    first commit

---
+++ Desktop/Testing app/Testing app/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Testing app
+//
+//  Created by Chanchal on 08/05/18.
+//  Copyright © 2018 Naxtre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Desktop/Testing app/Testing app/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Testing app
+//
+//  Created by Chanchal on 08/05/18.
+//  Copyright © 2018 Naxtre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
